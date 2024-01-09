784a29a6c20720d1170d730132eb3118f8a48ea1
MregXN <46479059+MregXN@users.noreply.github.com>
2024-01-09 21:52:58 +0800

    delay 12s to allow timer to fire
    Signed-off-by: MregXN <46479059+MregXN@users.noreply.github.com>

--- sdk-tests/src/test/java/io/dapr/it/actors/ActorTimerRecoveryIT.java
+++ sdk-tests/src/test/java/io/dapr/it/actors/ActorTimerRecoveryIT.java
@@ -84,13 +84,13 @@ public class ActorTimerRecoveryIT extends BaseIT {
     runs.left.stop();
 
     // Pause a bit to let placements settle.
-    logger.info("Pausing 12 seconds to let placements settle.");
-    Thread.sleep(Duration.ofSeconds(12).toMillis());
+    logger.info("Pausing 10 seconds to let placements settle.");
+    Thread.sleep(Duration.ofSeconds(10).toMillis());
 
     runs.left.start();
 
-    logger.debug("Pausing 10 seconds to allow timer to fire");
-    Thread.sleep(10000);
+    logger.debug("Pausing 12 seconds to allow timer to fire");
+    Thread.sleep(12000);
     final List<MethodEntryTracker> newLogs = new ArrayList<>();
     callWithRetry(() -> {
       newLogs.clear();
